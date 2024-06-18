ec4233d8e6ae1a66f4e946632baacca8d75a21da
Hojiakbar <hojiakbaribodullayev04@gmail.com>
2024-06-18 20:30:20 +0500

    web aplication with fast api and telegram bot

---
+++ tele_bot/data/config.py
@@ -0,0 +1,15 @@
+from environs import Env
+
+# environs kutubxonasidan foydalanish
+env = Env()
+env.read_env()
+
+# .env fayl ichidan quyidagilarni o'qiymiz
+BOT_TOKEN = env.str("BOT_TOKEN")  # Bot toekn
+ADMINS = env.list("ADMINS")  # adminlar ro'yxati
+IP = env.str("ip")  # Xosting ip manzili
+
+USER = env.str("USER")
+PASSWORD = env.str("PASSWORD")
+NAME = env.str("NAME")
+HOST = env.str("HOST")
